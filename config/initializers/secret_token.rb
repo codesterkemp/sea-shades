df635c789f1c576181b6770d213c7de609b85283
codesterkemp <sparky_csk@yahoo.com>
2013-04-11 12:19:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seashades::Application.config.secret_token = '8f6c57e30105c58c30f0b49135f603e38275c81c893809c57dcfacbda46848fd80f625e5dd836d0b46803b5ce3dc49fc00ebd9c69ccf9b471de8c30ff0496e27'
